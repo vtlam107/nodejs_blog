e905e879570fcfeef4ce4e2ecde0b9a61dc12606
vo tung lam <vteel07@gmail.com>
2021-12-19 14:54:59 +0700

    scss and bootstrap 4

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-
-
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const express = require('express')
+const morgan = require('morgan')
+const handlebars  = require('express-handlebars');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3000
 
-//HTTP logger
-app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//template engine
-app.engine(
-  'hbs',
-  handlebars({
-      extname: '.hbs',
-      helpers: {
-          sum: (a, b) => a + b,
-      },
-  }),
-);
+// HTTP loger
+app.use(morgan('combined'))
+// Template engine
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs'
+}));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// console.log('PATH: ', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, '\\resources\\views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-});
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-});
+})
